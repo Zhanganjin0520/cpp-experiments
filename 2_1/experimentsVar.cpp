4d4cee091a279ca9ff3924b071167bd648d99682
zhanganjin <zhanganjin0520@gmail.com>
2025-06-19 21:54:28 +0800

    提交2.1节所有实验

--- 2_1/experimentsVar.cpp
+++ 2_1/experimentsVar.cpp
@@ -1,5 +1,9 @@
 //
-// Created by zhanganjin on 2025/6/18.
+//实验笔记
+//左移运算 <<,左移后，低位补0，高位舍弃
+//右移运算 >>,右移后, 低位舍弃, 高位无符号数补零，有符号数补符号位
+// a = 0 || sizeof(int); 等价于 0||4 输出为 True
+// sizeof 占多少字节
 //
 
 #include "experimentsVar.h"
@@ -7,23 +11,21 @@
 using namespace std;
 
 int main() {
+    //实验1
     const double pi = 3.14159;
     cout << "定义常量Pi " << pi << endl;
 
-    //sizeof 占多少字节
-    int x = sizeof pi;
-    cout << "常量 PI 占用字节数" << x << endl;
+    int a = sizeof pi;
+    cout << "常量 PI 占用字节数\t" << a << endl;
 
-    x = sizeof(short);
-    cout << "short 类型占用字节数" << x << endl;
+    a = sizeof(short);
+    cout << "short 类型占用字节数\t" << a << endl;
 
-    x = 3 & 5;
-    cout << "3&5的结果是" << x << endl;
+    a = 3 & 5;
+    cout << "3&5的结果是\t" << a << endl;
 
-    x = sizeof(int);
-    cout << "int 类型占用字节数" << x << endl;
-
-    // a = 0 || sizeof(int); 输出位 True
+    a = sizeof(int);
+    cout << "int 类型占用字节数\t" << a << endl;
 
     double radius = 0;
     cout << "输入半径 radius" << "\n";
@@ -31,7 +33,26 @@ int main() {
 
     cout << "计算圆的周长" << 2 * pi * radius << endl;
 
-    //左移运算 <<,左移后，低位补0，高位舍弃
+    //实验2
+    //无符号整数
+    unsigned int x;
+    unsigned int y = 100;
+    unsigned int z = 50;
 
-    //右移运算 >>,右移后, 低位舍弃, 高位无符号数补零，有符号数补符号位
+    x = y - z;
+    cout << "Difference is: " << x << endl;
+    //无符号整数无法表示负数，x值为4294967246
+    x = z - y;
+    cout << "Difference is: " << x << endl;
+
+    //实验3
+    int i, j, k;
+    cout << "input value of i : \n";
+    cin >> i;
+
+    cout << "input value of j : \n";
+    cin >> j;
+
+    k = i - j > 0 ? i - j : j - i;
+    cout << "The difference of i and j is:\t" << k;
 }
