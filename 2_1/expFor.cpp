f9d651f6100e7875b27ce5e58bf17328b681b81b
zhanganjin <zhanganjin0520@gmail.com>
2025-07-13 15:25:40 +0700

    添加结构体

--- 2_1/expFor.cpp
+++ 2_1/expFor.cpp
@@ -7,17 +7,39 @@
 #include <iostream>
 using namespace std;
 
+enum GameResult { WIN, LOSE, TIE, CANCEL };
+
 //功能最强的循环语句 for 语句
 int main() {
     int n;
     cout << "Please enter a positive integer: ";
     cin >> n;
     cout << "Number " << n << " Factors ";
-    for (int k =1; k <= n; k++) {
+    for (int k = 1; k <= n; k++) {
         if (n % k == 0) {
             cout << k << " ";
         }
     }
     cout << endl;
+
+
+    GameResult result;
+    GameResult omit = CANCEL;
+    for (int count = WIN; count <= CANCEL; count++) {
+        result = GameResult(count);
+        if (result == omit) {
+            cout << "The game was cancelled" << endl;
+        } else {
+            cout << "The game was played ";
+            switch (result) {
+                case WIN: cout << "wins" << endl;
+                    break;
+                case LOSE: cout << "loses" << endl;
+                    break;
+                default: ;
+            }
+        }
+    }
+
     return 0;
 }
---
+++ 2_1/expStruct.h
@@ -0,0 +1,16 @@
+//
+// Created by zhanganjin on 2025/7/13.
+//
+
+#ifndef EXPSTRUCT_H
+#define EXPSTRUCT_H
+
+
+
+class expStruct {
+
+};
+
+
+
+#endif //EXPSTRUCT_H
